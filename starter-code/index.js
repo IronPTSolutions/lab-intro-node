98e1d32121df23e06927b55405de3d08897358c1
Julio García Pérez <juliojose.garcia@11paths.com>
2017-12-21 00:48:27 +0100

    solution

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,46 @@
-var SortedList = function() {};
+class SortedList {
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+  add(x) {
+    this.items.push(x);
+    this._sort();
+    this.length++;
+  }
+
+  get(i) {
+    return this.items[i - 1];
+  }
+
+  max() {
+    return this.get(this.length);
+  }
+
+  min() {
+    return this.get(1);
+  }
+
+  average() {
+    this._checkEmpty();
+    
+    return this.sum() / this.length;
+  }
+
+  sum() {
+    this._checkEmpty();
+
+    return this.items.reduce((acc, i) => { return acc + i; });
+  }
+
+  _sort() {
+    this.items = this.items.sort((a, b) => { return a - b; });
+  }
+
+  _checkEmpty() {
+    if (this.length === 0) throw new Error("EmptySortedList");
+  }
+}
 
 module.exports = SortedList;
